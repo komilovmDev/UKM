ffacbd60e10e346838b69741fef9d73d5e932d6e
Alibek <115139922+alibek-jumaniyazov@users.noreply.github.com>
2025-01-20 14:39:29 +0500

    add to back link

--- frontend/src/pages/Ustav/components/UstavTitle.jsx
+++ frontend/src/pages/Ustav/components/UstavTitle.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Icons } from "../../../images/icon";
+import { Link } from "react-router-dom";
 
-export default function UstavTitle({title}) {
+export default function UstavTitle({ title }) {
   return (
     <h1 className="text-[36px] text-[#ABBA27] flex justify-start items-center gap-5 font-medium leading-[63px]">
-      <Icons.UstavIconLeft /> {title}
+      <Link to={"/About"}>
+        <Icons.UstavIconLeft />{" "}
+      </Link>{" "}
+      {title}
     </h1>
   );
 }
